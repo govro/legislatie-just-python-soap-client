0bb3ff94956ef5735eb66b45d90d06aa50867265
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-09-01 14:00:19 +0300

    Results page

---
+++ client.py
@@ -0,0 +1,33 @@
+from suds.client import Client
+
+class LegislatieJustClient:
+    def __init__(self):
+        self.url = 'http://legislatie.just.ro/apiws/FreeWebService.svc?wsdl'
+        self.client = Client(self.url)
+        self.token = self.client.service.GetToken()
+
+    def search(self, params):
+        search_model = self.client.factory.create('SearchModel')
+        search_model.NumarPagina = params.get('NumarPagina', 0)
+        search_model.RezultatePagina = params.get('RezultatePagina', 0)
+
+        if 'SearchAn' in params:
+            search_model.SearchAn = params['SearchAn']
+
+        if 'SearchTitlu' in params:
+            search_model.SearchTitlu = params['SearchTitlu']
+
+        if 'SearchNumar' in params:
+            search_model.SearchNumar = params['SearchNumar']
+
+        if 'SearchText' in params:
+            search_model.SearchText = params['SearchText']
+
+        return self.client.service.Search(search_model, self.token)
+
+if __name__ == '__main__':
+    client = LegislatieJustClient()
+    results = client.search()
+
+    for law in results.Legi:
+        print("{0}\n{1}\n\n".format(law.Titlu, law.DataVigoare))
--- server.py
+++ server.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from client import LegislatieJustClient
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
 @app.route("/search", methods=['POST'])
-def search(params):
-    return params
+def search():
+    client = LegislatieJustClient()
+    params = {v: request.form[v] for k, v in enumerate(request.form)}
+    results = client.search(params).Legi
+    return render_template('search.html', laws=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
