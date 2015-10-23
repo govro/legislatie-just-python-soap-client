07cab2f2e3bbb95d60acad76edb1cc121d0830ca
Cristian Banu <cristb@gmail.com>
2015-10-23 22:23:17 +0300

    Enhanced the script to allow for multiple pages to be downloaded
    Fixed issue with expired token

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from suds.client import Client
+import time
 
 url = 'http://legislatie.just.ro/apiws/FreeWebService.svc?wsdl'
 
@@ -14,10 +15,26 @@ print(token)
 
 print("==== do search")
 search_model = client.factory.create('SearchModel')
-search_model.NumarPagina = 0
-search_model.RezultatePagina = 0
 
-results = client.service.Search(search_model, token)
+NUM_PAGES = 1000
+path = 'saved_results.' + str(int(time.time())) + '.txt';
 
-for law in results.Legi:
-    print("{0}\n{1}\n\n".format(law.Titlu, law.DataVigoare))
+print("==== saving results to {0}\n".format(path))
+
+with open(path, 'w') as f:
+    for i in range(NUM_PAGES):
+        search_model.NumarPagina = i
+        search_model.RezultatePagina = 0
+
+        try:
+            results = client.service.Search(search_model, token)
+        except suds.WebFault:
+            token = client.service.GetToken()
+            results = client.service.Search(search_model, token)
+
+        if len(results) == 0:
+            print("==== ! empty results at page {0}\n".format(i))
+        else:
+            print("==== fetching {0} page\n".format(i))
+            for law in results.Legi:
+                f.write("{0}\n{1}\n\n".format(law.Titlu, law.DataVigoare))
